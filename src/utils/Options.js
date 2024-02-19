6df1a73d3c614d9f28701779904f0eba896ca412
Pedro Negrao <macedopedro117@yahoo.com.br>
2024-02-19 13:47:10 -0300

    Criação de um novo arquivo para renderizar as options | Mudança no nome do component spinner para loading

---
+++ src/utils/Options.js
@@ -0,0 +1,986 @@
+export const arrOptions = [
+  {
+    id: 0,
+    value: "",
+    name: "Escolha uma moeda",
+  },
+  {
+    id: 1,
+    value: "AED",
+    name: "Dirham dos Emirados Árabes Unidos",
+  },
+  {
+    id: 2,
+    value: "AFN",
+    name: "Afegão afegão",
+  },
+  {
+    id: 3,
+    value: "ALL",
+    name: "Lek albanês",
+  },
+  {
+    id: 4,
+    value: "AMD",
+    name: "Dram Armênio",
+  },
+  {
+    id: 5,
+    value: "ANG",
+    name: "Florim das Antilhas Holandesas",
+  },
+  {
+    id: 6,
+    value: "AOA",
+    name: "Kwanza angolano",
+  },
+  {
+    id: 7,
+    value: "ARS",
+    name: "Peso Argentino",
+  },
+  {
+    id: 8,
+    value: "AUD",
+    name: "Dólar australiano",
+  },
+  {
+    id: 9,
+    value: "AWG",
+    name: "Florim arubano",
+  },
+  {
+    id: 10,
+    value: "AZN",
+    name: "Manat do Azerbaijão",
+  },
+  {
+    id: 11,
+    value: "BAM",
+    name: "Marco da Bósnia e Herzegovina",
+  },
+  {
+    id: 12,
+    value: "BBD",
+    name: "Dólar de Barbados",
+  },
+  {
+    id: 13,
+    value: "BDT",
+    name: "Taka de Bangladesh",
+  },
+  {
+    id: 14,
+    value: "BGN",
+    name: "Lev Búlgaro",
+  },
+  {
+    id: 15,
+    value: "BHD",
+    name: "Dinar do Bahrein",
+  },
+  {
+    id: 16,
+    value: "BIF",
+    name: "Franco Burundiano",
+  },
+  {
+    id: 17,
+    value: "BMD",
+    name: "Dólar das Bermudas",
+  },
+  {
+    id: 18,
+    value: "BND",
+    name: "Dólar de Brunei",
+  },
+  {
+    id: 19,
+    value: "BOB",
+    name: "Boliviano boliviano",
+  },
+  {
+    id: 20,
+    value: "BRL",
+    name: "Real Brasileiro",
+  },
+  {
+    id: 21,
+    value: "BSD",
+    name: "Dólar das Bahamas",
+  },
+  {
+    id: 22,
+    value: "BTN",
+    name: "Ngultrum butanês",
+  },
+  {
+    id: 23,
+    value: "BWP",
+    name: "Pula do Botsuana",
+  },
+  {
+    id: 24,
+    value: "BYN",
+    name: "Rublo Bielorrusso",
+  },
+  {
+    id: 25,
+    value: "BZD",
+    name: "Dólar Belizenho",
+  },
+  {
+    id: 26,
+    value: "CAD",
+    name: "Dólar Canadense",
+  },
+  {
+    id: 27,
+    value: "CDF",
+    name: "Franco Congolês",
+  },
+  {
+    id: 28,
+    value: "CHF",
+    name: "Franco Suíço",
+  },
+  {
+    id: 29,
+    value: "CLP",
+    name: "Peso Chileno",
+  },
+  {
+    id: 30,
+    value: "CNY",
+    name: "Renminbi Chinês",
+  },
+  {
+    id: 31,
+    value: "COP",
+    name: "Peso Colombiano",
+  },
+  {
+    id: 32,
+    value: "CRC",
+    name: "Cólon Costarriquenho",
+  },
+  {
+    id: 33,
+    value: "CUP",
+    name: "Peso Cubano",
+  },
+  {
+    id: 34,
+    value: "CVE",
+    name: "Escudo Cabo-verdiano",
+  },
+  {
+    id: 35,
+    value: "CZK",
+    name: "Coroa Tcheca",
+  },
+  {
+    id: 36,
+    value: "DJF",
+    name: "Franco Djibutiano",
+  },
+  {
+    id: 37,
+    value: "DKK",
+    name: "Coroa Dinamarquesa",
+  },
+  {
+    id: 38,
+    value: "DOP",
+    name: "Peso Dominicano",
+  },
+  {
+    id: 39,
+    value: "DZD",
+    name: "Dinar Argelino",
+  },
+  {
+    id: 40,
+    value: "EGP",
+    name: "Libra Egípcia",
+  },
+  {
+    id: 41,
+    value: "ERN",
+    name: "Nakfa da Eritreia",
+  },
+  {
+    id: 42,
+    value: "ETB",
+    name: "Birr Etíope",
+  },
+  {
+    id: 43,
+    value: "EUR",
+    name: "Euro",
+  },
+  {
+    id: 44,
+    value: "FJD",
+    name: "Dólar de Fiji",
+  },
+  {
+    id: 45,
+    value: "FKP",
+    name: "Libra das Ilhas Malvinas",
+  },
+  {
+    id: 46,
+    value: "FOK",
+    name: "Coroa Faroense",
+  },
+  {
+    id: 47,
+    value: "GBP",
+    name: "Libra Esterlina",
+  },
+  {
+    id: 48,
+    value: "GEL",
+    name: "Lari Georgiano",
+  },
+  {
+    id: 49,
+    value: "GGP",
+    name: "Libra de Guernsey",
+  },
+  {
+    id: 50,
+    value: "GHS",
+    name: "Cedi Ganês",
+  },
+  {
+    id: 51,
+    value: "GIP",
+    name: "Libra de Gibraltar",
+  },
+  {
+    id: 52,
+    value: "GMD",
+    name: "Dalasi Gambiano",
+  },
+  {
+    id: 53,
+    value: "GNF",
+    name: "Franco Guineense",
+  },
+  {
+    id: 54,
+    value: "GTQ",
+    name: "Quetzal da Guatemala",
+  },
+  {
+    id: 55,
+    value: "GYD",
+    name: "Dólar Guianense",
+  },
+  {
+    id: 56,
+    value: "HKD",
+    name: "Dólar de Hong Kong",
+  },
+  {
+    id: 57,
+    value: "HNL",
+    name: "Lempira Hondurenha",
+  },
+  {
+    id: 58,
+    value: "HRK",
+    name: "Kuna Croata",
+  },
+  {
+    id: 59,
+    value: "HTG",
+    name: "Gourde Haitiano",
+  },
+  {
+    id: 60,
+    value: "HUF",
+    name: "Forint Húngaro",
+  },
+  {
+    id: 61,
+    value: "IDR",
+    name: "Rupia Indonésia",
+  },
+  {
+    id: 62,
+    value: "ILS",
+    name: "Novo Shekel Israelense",
+  },
+  {
+    id: 63,
+    value: "IMP",
+    name: "Libra Manx",
+  },
+  {
+    id: 64,
+    value: "INR",
+    name: "Rupia Indiana",
+  },
+  {
+    id: 65,
+    value: "IQD",
+    name: "Dinar Iraquiano",
+  },
+  {
+    id: 66,
+    value: "IRR",
+    name: "Rial Iraniano",
+  },
+  {
+    id: 67,
+    value: "ISK",
+    name: "Coroa Islandesa",
+  },
+  {
+    id: 68,
+    value: "JEP",
+    name: "Libra de Jersey",
+  },
+  {
+    id: 69,
+    value: "JMD",
+    name: "Dólar Jamaicano",
+  },
+  {
+    id: 70,
+    value: "JOD",
+    name: "Dinar Jordaniano",
+  },
+  {
+    id: 71,
+    value: "JPY",
+    name: "Yen Japonês",
+  },
+  {
+    id: 72,
+    value: "KES",
+    name: "Xelim Queniano",
+  },
+  {
+    id: 73,
+    value: "KGS",
+    name: "Som do Quirguistão",
+  },
+  {
+    id: 74,
+    value: "KHR",
+    name: "Riel Cambojano",
+  },
+  {
+    id: 75,
+    value: "KID",
+    name: "Dólar Quiribati",
+  },
+  {
+    id: 76,
+    value: "KMF",
+    name: "Franco Comoriano",
+  },
+  {
+    id: 77,
+    value: "KRW",
+    name: "Won Sul-Coreano",
+  },
+  {
+    id: 78,
+    value: "KWD",
+    name: "Dinar do Kuwait",
+  },
+  {
+    id: 79,
+    value: "KYD",
+    name: "Dólar das Ilhas Cayman",
+  },
+  {
+    id: 80,
+    value: "KZT",
+    name: "Tenge do Cazaquistão",
+  },
+  {
+    id: 81,
+    value: "LAK",
+    name: "Laos Kip",
+  },
+  {
+    id: 82,
+    value: "LBP",
+    name: "Libra Libanesa",
+  },
+  {
+    id: 83,
+    value: "LKR",
+    name: "Rupia do Sri Lanka",
+  },
+  {
+    id: 84,
+    value: "LRD",
+    name: "Dólar da Libéria",
+  },
+  {
+    id: 85,
+    value: "LSL",
+    name: "Lesoto Loti",
+  },
+  {
+    id: 86,
+    value: "LYD",
+    name: "Dinar Líbio",
+  },
+  {
+    id: 87,
+    value: "MAD",
+    name: "Dirham Marroquino",
+  },
+  {
+    id: 88,
+    value: "MDL",
+    name: "Leu da Moldávia",
+  },
+  {
+    id: 89,
+    value: "MGA",
+    name: "Ariary Malgaxe",
+  },
+  {
+    id: 90,
+    value: "MKD",
+    name: "Denar Macedônio",
+  },
+  {
+    id: 91,
+    value: "MMK",
+    name: "Kyat Birmanês",
+  },
+  {
+    id: 92,
+    value: "MNT",
+    name: "Tögrög da Mongólia",
+  },
+  {
+    id: 93,
+    value: "MOP",
+    name: "Pataca Macaense",
+  },
+  {
+    id: 94,
+    value: "MRU",
+    name: "Ouguiya da Mauritânia",
+  },
+  {
+    id: 95,
+    value: "MUR",
+    name: "Rupia das Maurícias",
+  },
+  {
+    id: 96,
+    value: "MVR",
+    name: "Rufiyaa das Maldivas",
+  },
+  {
+    id: 97,
+    value: "MWK",
+    name: "Kwacha do Malawi",
+  },
+  {
+    id: 98,
+    value: "MXN",
+    name: "Peso Mexicano",
+  },
+  {
+    id: 99,
+    value: "MYR",
+    name: "Ringgit Malaio",
+  },
+  {
+    id: 100,
+    value: "MZN",
+    name: "Metical Moçambicano",
+  },
+  {
+    id: 101,
+    value: "NAD",
+    name: "Dólar da Namíbia",
+  },
+  {
+    id: 102,
+    value: "NGN",
+    name: "Naira Nigeriana",
+  },
+  {
+    id: 103,
+    value: "NIO",
+    name: "Córdoba Nicaraguense",
+  },
+  {
+    id: 104,
+    value: "NOK",
+    name: "Coroa Norueguesa",
+  },
+  {
+    id: 105,
+    value: "NPR",
+    name: "Rúpia Nepalesa",
+  },
+  {
+    id: 106,
+    value: "NZD",
+    name: "Dólar da Nova Zelândia",
+  },
+  {
+    id: 107,
+    value: "OMR",
+    name: "Rial de Omã",
+  },
+  {
+    id: 108,
+    value: "PAB",
+    name: "Balboa Panamenho",
+  },
+  {
+    id: 109,
+    value: "PEN",
+    name: "Sol Peruano",
+  },
+  {
+    id: 110,
+    value: "PGK",
+    name: "Kina da Papua Nova Guiné",
+  },
+  {
+    id: 111,
+    value: "PHP",
+    name: "Peso Filipino",
+  },
+  {
+    id: 112,
+    value: "PKR",
+    name: "Rupia Paquistanesa",
+  },
+  {
+    id: 113,
+    value: "PLN",
+    name: "Zloty Polonês",
+  },
+  {
+    id: 114,
+    value: "PYG",
+    name: "Guarani Paraguaio",
+  },
+  {
+    id: 115,
+    value: "QAR",
+    name: "Rial do Catar",
+  },
+  {
+    id: 116,
+    value: "RON",
+    name: "Leu Romeno",
+  },
+  {
+    id: 117,
+    value: "RSD",
+    name: "Dinar Sérvio",
+  },
+  {
+    id: 118,
+    value: "RUB",
+    name: "Rublo Russo",
+  },
+  {
+    id: 119,
+    value: "RWF",
+    name: "Franco Ruandês",
+  },
+  {
+    id: 120,
+    value: "SAR",
+    name: "Rial Saudita",
+  },
+  {
+    id: 121,
+    value: "SBD",
+    name: "Dólar das Ilhas Salomão",
+  },
+  {
+    id: 122,
+    value: "SCR",
+    name: "Rupia Seichelense",
+  },
+  {
+    id: 123,
+    value: "SDG",
+    name: "Libra Sudanesa",
+  },
+  {
+    id: 124,
+    value: "SEK",
+    name: "Coroa Sueca",
+  },
+  {
+    id: 125,
+    value: "SGD",
+    name: "Dólar de Singapura",
+  },
+  {
+    id: 126,
+    value: "SHP",
+    name: "Libra de Santa Helena",
+  },
+  {
+    id: 127,
+    value: "SLE",
+    name: "Leoa da Serra Leoa",
+  },
+  {
+    id: 128,
+    value: "SOS",
+    name: "Xelim Somali",
+  },
+  {
+    id: 129,
+    value: "SRD",
+    name: "Dólar do Suriname",
+  },
+  {
+    id: 130,
+    value: "SSP",
+    name: "Libra do Sudão do Sul",
+  },
+  {
+    id: 131,
+    value: "STN",
+    name: "São Tomé e Príncipe Dobra",
+  },
+  {
+    id: 132,
+    value: "SYP",
+    name: "Libra Síria",
+  },
+  {
+    id: 133,
+    value: "SZL",
+    name: "Essuatíni Lilangeni",
+  },
+  {
+    id: 134,
+    value: "THB",
+    name: "Baht Tailandês",
+  },
+  {
+    id: 135,
+    value: "TJS",
+    name: "Somoni Tadjique",
+  },
+  {
+    id: 136,
+    value: "TMT",
+    name: "Manat do Turquemenistão",
+  },
+  {
+    id: 137,
+    value: "TND",
+    name: "Dinar Tunisino",
+  },
+  {
+    id: 138,
+    value: "TOP",
+    name: "Pa'anga de Tonga",
+  },
+  {
+    id: 139,
+    value: "TRY",
+    name: "Lira Turca",
+  },
+  {
+    id: 140,
+    value: "TTD",
+    name: "Dólar de Trinidad e Tobago",
+  },
+  {
+    id: 141,
+    value: "TVD",
+    name: "Dólar Tuvaluano",
+  },
+  {
+    id: 142,
+    value: "TWD",
+    name: "Novo dólar Taiwanês",
+  },
+  {
+    id: 143,
+    value: "TZS",
+    name: "Xelim da Tanzânia",
+  },
+  {
+    id: 144,
+    value: "UAH",
+    name: "Hryvnia Ucraniana",
+  },
+  {
+    id: 145,
+    value: "UGX",
+    name: "Xelim Ugandês",
+  },
+  {
+    id: 146,
+    value: "USD",
+    name: "Dólar dos Estados Unidos",
+  },
+  {
+    id: 147,
+    value: "UYU",
+    name: "Peso Uruguaio",
+  },
+  {
+    id: 148,
+    value: "UZS",
+    name: "Sou do Uzbequistão",
+  },
+  {
+    id: 149,
+    value: "VES",
+    name: "Bolívar Soberano Venezuelano",
+  },
+  {
+    id: 150,
+    value: "VND",
+    name: "Dong Vietnamita",
+  },
+  {
+    id: 151,
+    value: "VUV",
+    name: "Vatu de Vanuatu",
+  },
+  {
+    id: 152,
+    value: "WST",
+    name: "Tala Samoana",
+  },
+  {
+    id: 153,
+    value: "XAF",
+    name: "Franco CFA da África Central",
+  },
+  {
+    id: 154,
+    value: "XCD",
+    name: "Dólar do Caribe Oriental",
+  },
+  {
+    id: 155,
+    value: "XDR",
+    name: "Direitos de Saque Especiais",
+  },
+  {
+    id: 156,
+    value: "XOF",
+    name: "Franco CFA da África Ocidental",
+  },
+  {
+    id: 157,
+    value: "XPF",
+    name: "Franco CFP",
+  },
+  {
+    id: 158,
+    value: "YER",
+    name: "Rial Iemenita",
+  },
+  {
+    id: 159,
+    value: "ZAR",
+    name: "Rand Sul-Africano",
+  },
+  {
+    id: 160,
+    value: "ZMW",
+    name: "Kwacha da Zâmbia",
+  },
+  {
+    id: 161,
+    value: "ZWL",
+    name: "Dólar do Zimbábue",
+  },
+];
+
+{
+  /* <select
+id="base"
+name="base"
+required
+autoFocus
+onChange={handleSelect}
+onClick={cleanResult}
+>
+<option value="">Escolha uma moeda</option>
+<option value="AED">Dirham dos Emirados Árabes Unidos</option>
+<option value="AFN">Afegão afegão</option>
+<option value="ALL">Lek albanês</option>
+<option value="AMD">Dram Armênio</option>
+<option value="ANG">Florim das Antilhas Holandesas</option>
+<option value="AOA">Kwanza angolano</option>
+<option value="ARS">Peso Argentino</option>
+<option value="AUD">Dólar australiano</option>
+<option value="AWG">Florim arubano</option>
+<option value="AZN">Manat do Azerbaijão</option>
+<option value="BAM">Marco da Bósnia e Herzegovina</option>
+<option value="BBD">Dólar de Barbados</option>
+<option value="BDT">Taka de Bangladesh</option>
+<option value="BGN">Lev Búlgaro</option>
+<option value="BHD">Dinar do Bahrein</option>
+<option value="BIF">Franco Burundiano</option>
+<option value="BMD">Dólar das Bermudas</option>
+<option value="BND">Dólar de Brunei</option>
+<option value="BOB">Boliviano boliviano</option>
+<option value="BRL">Real Brasileiro</option>
+<option value="BSD">Dólar das Bahamas</option>
+<option value="BTN">Ngultrum butanês</option>
+<option value="BWP">Pula do Botsuana</option>
+<option value="BYN">Rublo Bielorrusso</option>
+<option value="BZD">Dólar Belizenho</option>
+<option value="CAD">Dólar Canadense</option>
+<option value="CDF">Franco Congolês</option>
+<option value="CHF">Franco Suíço</option>
+<option value="CLP">Peso Chileno</option>
+<option value="CNY">Renminbi Chinês</option>
+<option value="COP">Peso Colombiano</option>
+<option value="CRC">Cólon Costarriquenho</option>
+<option value="CUP">Peso Cubano</option>
+<option value="CVE">Escudo Cabo-verdiano</option>
+<option value="CZK">Coroa Tcheca</option>
+<option value="DJF">Franco Djibutiano</option>
+<option value="DKK">Coroa Dinamarquesa</option>
+<option value="DOP">Peso Dominicano</option>
+<option value="DZD">Dinar Argelino</option>
+<option value="EGP">Libra Egípcia</option>
+<option value="ERN">Nakfa da Eritreia</option>
+<option value="ETB">Birr Etíope</option>
+<option value="EUR">Euro</option>
+<option value="FJD">Dólar de Fiji</option>
+<option value="FKP">Libra das Ilhas Malvinas</option>
+<option value="FOK">Coroa Faroense</option>
+<option value="GBP">Libra Esterlina</option>
+<option value="GEL">Lari Georgiano</option>
+<option value="GGP">Libra de Guernsey</option>
+<option value="GHS">Cedi Ganês</option>
+<option value="GIP">Libra de Gibraltar</option>
+<option value="GMD">Dalasi Gambiano</option>
+<option value="GNF">Franco Guineense</option>
+<option value="GTQ">Quetzal da Guatemala</option>
+<option value="GYD">Dólar Guianense</option>
+<option value="HKD">Dólar de Hong Kong</option>
+<option value="HNL">Lempira Hondurenha</option>
+<option value="HRK">Kuna Croata</option>
+<option value="HTG">Gourde Haitiano</option>
+<option value="HUF">Forint Húngaro</option>
+<option value="IDR">Rupia Indonésia</option>
+<option value="ILS">Novo Shekel Israelense</option>
+<option value="IMP">Libra Manx</option>
+<option value="INR">Rupia Indiana</option>
+<option value="IQD">Dinar Iraquiano</option>
+<option value="IRR">Rial Iraniano</option>
+<option value="ISK">Coroa Islandesa</option>
+<option value="JEP">Libra de Jersey</option>
+<option value="JMD">Dólar Jamaicano</option>
+<option value="JOD">Dinar Jordaniano</option>
+<option value="JPY">Yen Japonês</option>
+<option value="KES">Xelim Queniano</option>
+<option value="KGS">Som do Quirguistão</option>
+<option value="KHR">Riel Cambojano</option>
+<option value="KID">Dólar Quiribati</option>
+<option value="KMF">Franco Comoriano</option>
+<option value="KRW">Won Sul-Coreano</option>
+<option value="KWD">Dinar do Kuwait</option>
+<option value="KYD">Dólar das Ilhas Cayman</option>
+<option value="KZT">Tenge do Cazaquistão</option>
+<option value="LAK">Laos Kip</option>
+<option value="LBP">Libra Libanesa</option>
+<option value="LKR">Rupia do Sri Lanka</option>
+<option value="LRD">Dólar da Libéria</option>
+<option value="LSL">Lesoto Loti</option>
+<option value="LYD">Dinar Líbio</option>
+<option value="MAD">Dirham Marroquino</option>
+<option value="MDL">Leu da Moldávia</option>
+<option value="MGA">Ariary Malgaxe</option>
+<option value="MKD">Denar Macedônio</option>
+<option value="MMK">Kyat Birmanês</option>
+<option value="MNT">Tögrög da Mongólia</option>
+<option value="MOP">Pataca Macaense</option>
+<option value="MRU">Ouguiya da Mauritânia</option>
+<option value="MUR">Rupia das Maurícias</option>
+<option value="MVR">Rufiyaa das Maldivas</option>
+<option value="MWK">Kwacha do Malawi</option>
+<option value="MXN">Peso Mexicano</option>
+<option value="MYR">Ringgit Malaio</option>
+<option value="MZN">Metical Moçambicano</option>
+<option value="NAD">Dólar da Namíbia</option>
+<option value="NGN">Naira Nigeriana</option>
+<option value="NIO">Córdoba Nicaraguense</option>
+<option value="NOK">Coroa Norueguesa</option>
+<option value="NPR">Rúpia Nepalesa</option>
+<option value="NZD">Dólar da Nova Zelândia</option>
+<option value="OMR">Rial de Omã</option>
+<option value="PAB">Balboa Panamenho</option>
+<option value="PEN">Sol Peruano</option>
+<option value="PGK">Kina da Papua Nova Guiné</option>
+<option value="PHP">Peso Filipino</option>
+<option value="PKR">Rupia Paquistanesa</option>
+<option value="PLN">Zloty Polonês</option>
+<option value="PYG">Guarani Paraguaio</option>
+<option value="QAR">Rial do Catar</option>
+<option value="RON">Leu Romeno</option>
+<option value="RSD">Dinar Sérvio</option>
+<option value="RUB">Rublo Russo</option>
+<option value="RWF">Franco Ruandês</option>
+<option value="SAR">Rial Saudita</option>
+<option value="SBD">Dólar das Ilhas Salomão</option>
+<option value="SCR">Rupia Seichelense</option>
+<option value="SDG">Libra Sudanesa</option>
+<option value="SEK">Coroa Sueca</option>
+<option value="SGD">Dólar de Singapura</option>
+<option value="SHP">Libra de Santa Helena</option>
+<option value="SLE">Leoa da Serra Leoa</option>
+<option value="SOS">Xelim Somali</option>
+<option value="SRD">Dólar do Suriname</option>
+<option value="SSP">Libra do Sudão do Sul</option>
+<option value="STN">São Tomé e Príncipe Dobra</option>
+<option value="SYP">Libra Síria</option>
+<option value="SZL">Essuatíni Lilangeni</option>
+<option value="THB">Baht Tailandês</option>
+<option value="TJS">Somoni Tadjique</option>
+<option value="TMT">Manat do Turquemenistão</option>
+<option value="TND">Dinar Tunisino</option>
+<option value="TOP">Pa'anga de Tonga</option>
+<option value="TRY">Lira Turca</option>
+<option value="TTD">Dólar de Trinidad e Tobago</option>
+<option value="TVD">Dólar Tuvaluano</option>
+<option value="TWD">Novo dólar Taiwanês</option>
+<option value="TZS">Xelim da Tanzânia</option>
+<option value="UAH">Hryvnia Ucraniana</option>
+<option value="UGX">Xelim Ugandês</option>
+<option value="USD">Dólar dos Estados Unidos</option>
+<option value="UYU">Peso Uruguaio</option>
+<option value="UZS">Sou do Uzbequistão</option>
+<option value="VES">Bolívar Soberano Venezuelano</option>
+<option value="VND">Dong Vietnamita</option>
+<option value="VUV">Vatu de Vanuatu</option>
+<option value="WST">Tala Samoana</option>
+<option value="XAF">Franco CFA da África Central</option>
+<option value="XCD">Dólar do Caribe Oriental</option>
+<option value="XDR">Direitos de Saque Especiais</option>
+<option value="XOF">Franco CFA da África Ocidental</option>
+<option value="XPF">Franco CFP</option>
+<option value="YER">Rial Iemenita</option>
+<option value="ZAR">Rand Sul-Africano</option>
+<option value="ZMW">Kwacha da Zâmbia</option>
+<option value="ZWL">Dólar do Zimbábue</option>
+</select> */
+}
